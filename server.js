bfcb13a059877f1f25ce1e9344ceada5fbcb65a3
AIPB07 <binyon.aip@gmail.com>
2020-02-07 10:35:43 +0000

    Allow CORS

--- server.js
+++ server.js
@@ -23,6 +23,23 @@ mongoose.connect(mongoURI, {
 }).then(() => console.log('Successfully connected to MongoDB'))
 .catch(err => console.log(err));
 
+// Allow CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if (req.method === 'OPTIONS') {
+        res.header(
+            'Access-Control-Allow-Methods',
+            'PUT, POST, PATCH, DELETE, GET, UPDATE'
+        )
+        return res.status(200).json({});
+    }
+    next();
+});
+
 // Routes
 app.use('/api/posts', posts);
 
